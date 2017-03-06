53a8f97ef9b4f61ae83ed2241cce3f939462ee0e
ywang862 <henryw@gatech.edu>
2017-03-06 01:46:44 -0500

    nothing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meetup-for-Fitness
+CS4261 Project_An user-friendly application for users to organize and join local sports activities
---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request, abort
+from flask.ext.mysql import MySQL
+import json, codecs
+import boto3
+from werkzeug.utils import secure_filename
+import datetime
+
+config = json.load(codecs.open('config.json', encoding='utf-8'))
+app = Flask(__name__)
+mysql = MySQL()
+client = boto3.client('s3')
+
+app.config['MYSQL_DATABASE_USER'] = config['db_user']
+app.config['MYSQL_DATABASE_PASSWORD'] = config['db_passwd']
+app.config['MYSQL_DATABASE_DB'] = config['db_db']
+app.config['MYSQL_DATABASE_HOST'] = config['db_host']
+mysql.init_app(app)
